0e5fc13da21f539334684b840e116041c646be57
NURQUILLA <nurquilla@ggc.edu>
2022-04-28 13:25:08 -0400

    commit
    xommit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -122,63 +122,11 @@ search( event ){
 selectedAnt(key){
   console.log(key);
   return this.selectedAacterium;
-
-  userRecords = [{
-    "id": 1,
-    "name": "Leanne Graham",
-    "email": "Sincere@april.biz"
-  },
-  {
-    "id": 2,
-    "name": "Ervin Howell",
-    "email": "Shanna@melissa.tv"
-  },
-  {
-    "id": 3,
-    "name": "Clementine Bauch",
-    "email": "Nathan@yesenia.net"
-  },
-  {
-    "id": 4,
-    "name": "Patricia Lebsack",
-    "email": "Julianne.OConner@kory.org"
-  },
-  {
-    "id": 5,
-    "name": "Chelsey Dietrich",
-    "email": "Lucio_Hettinger@annie.ca"
-  },
-  {
-    "id": 6,
-    "name": "Mrs. Dennis Schulist",
-    "email": "Karley_Dach@jasper.info"
-  },
-  {
-    "id": 7,
-    "name": "Kurtis Weissnat",
-    "email": "Telly.Hoeger@billy.biz"
-  },
-  {
-    "id": 8,
-    "name": "Nicholas Runolfsdottir V",
-    "email": "Sherwood@rosamond.me"
-  },
-  {
-    "id": 9,
-    "name": "Glenna Reichert",
-    "email": "Chaim_McDermott@dana.io"
-  },
-  {
-    "id": 10,
-    "name": "Clementina DuBuque",
-    "email": "Rey.Padberg@karina.biz"
-  }
-]
 }
 
   //Author: Jeremy Stiff jstiff@ggc.edu
   //Function to check if localStorage is enabled on user browser
-  private checkLocal(): boolean {
+   checkLocal(): boolean {
     try {
       const key = '__testing key to determine if localstorage is enabled__';
       localStorage.setItem(key, key);
